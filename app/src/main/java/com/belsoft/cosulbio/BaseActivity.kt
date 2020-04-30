aa76cd2ce774f0d96d794609fc9e4e7bc92714e3
salox <ciprian.salomir@gmail.com>
2020-04-30 15:05:46 +0300

    Lifecycle debug code cleanup.

--- app/src/main/java/com/belsoft/cosulbio/BaseActivity.kt
+++ app/src/main/java/com/belsoft/cosulbio/BaseActivity.kt
@@ -11,38 +11,7 @@ abstract class BaseActivity : AppCompatActivity() {
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "${javaClass.simpleName} OnCreate")
         super.onCreate(savedInstanceState)
-//        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-    }
-
-    override fun onStart() {
-        Log.d(TAG, "${javaClass.simpleName} OnStart")
-        super.onStart()
-    }
-
-    override fun onResume() {
-        Log.d(TAG, "${javaClass.simpleName} OnResume")
-        super.onResume()
-    }
-
-    override fun onRestart() {
-        Log.d(TAG, "${javaClass.simpleName} OnRestart")
-        super.onRestart()
-    }
-
-    override fun onPause() {
-        Log.d(TAG, "${javaClass.simpleName} OnPause")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d(TAG, "${javaClass.simpleName} OnStop")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "${javaClass.simpleName} OnDestroy")
-        super.onDestroy()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
 }
--- app/src/main/java/com/belsoft/cosulbio/BaseFragment.kt
+++ app/src/main/java/com/belsoft/cosulbio/BaseFragment.kt
@@ -1,18 +1,12 @@
 package com.belsoft.cosulbio
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.belsoft.cosulbio.utils.InjectorUtils
-import com.belsoft.cosulbio.utils.TAG
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.cancel
 
 abstract class BaseFragment : Fragment() {
 
@@ -20,16 +14,6 @@ abstract class BaseFragment : Fragment() {
     lateinit var fgmView: View
     val localScope: CoroutineScope = MainScope()
     var isRunning: Boolean = false
-
-    override fun onAttach(context: Context) {
-        Log.d(TAG, "${javaClass.simpleName} onAttach")
-        super.onAttach(context)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "${javaClass.simpleName} onCreate")
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -40,21 +24,5 @@ abstract class BaseFragment : Fragment() {
         // Use ViewModelProviders class to create / get already created QuotesViewModel
         // for this view (activity)
         mainViewModel = ViewModelProvider(requireActivity(), factory).get(MainViewModel::class.java)
-    }
-
-    override fun onDestroyView() {
-        Log.d(TAG, "${javaClass.simpleName} onDestroyView")
-        super.onDestroyView()
-        localScope.cancel()
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "${javaClass.simpleName} onDestroy")
-        super.onDestroy()
-    }
-
-    override fun onDetach() {
-        Log.d(TAG, "${javaClass.simpleName} onDetach")
-        super.onDetach()
     }
 }
