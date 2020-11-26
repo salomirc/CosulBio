5f277aaf5fbf04bc2b4ef34583478209bedeca96
c9012832 <Ciprian.Salomir@amgreetings.com>
2020-11-26 13:09:26 +0200

    ViewModelFactory.kt constructor update.

--- app/src/main/java/com/belsoft/cosulbio/utils/ViewModelFactory.kt
+++ app/src/main/java/com/belsoft/cosulbio/utils/ViewModelFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.belsoft.cosulbio.database.IDbRepository
 import com.belsoft.cosulbio.services.IRequestHelper
 
-class ViewModelFactory<T : ViewModel?>(private val factory: () -> T) : ViewModelProvider.NewInstanceFactory(){
+class ViewModelFactory<T : ViewModel?> constructor (private val factory: () -> T) : ViewModelProvider.NewInstanceFactory(){
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
