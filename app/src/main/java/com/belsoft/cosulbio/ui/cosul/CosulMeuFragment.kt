14da917fbf38eed37754e98b7b8682dde193bd61
Ciprian Salomir <ciprian.salomir@gmail.com>
2024-11-13 19:47:39 +0200

    update project to replace obsolete dependencies

--- app/src/main/java/com/belsoft/cosulbio/ui/cosul/CosulMeuFragment.kt
+++ app/src/main/java/com/belsoft/cosulbio/ui/cosul/CosulMeuFragment.kt
@@ -1,12 +1,11 @@
 package com.belsoft.cosulbio.ui.cosul
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.belsoft.cosulbio.R
 
 class CosulMeuFragment : Fragment() {
@@ -26,7 +25,7 @@ class CosulMeuFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CosulMeuViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(CosulMeuViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
--- app/src/main/java/com/belsoft/cosulbio/ui/orders/MyOrdersFragment.kt
+++ app/src/main/java/com/belsoft/cosulbio/ui/orders/MyOrdersFragment.kt
@@ -1,12 +1,11 @@
 package com.belsoft.cosulbio.ui.orders
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.belsoft.cosulbio.R
 
 class MyOrdersFragment : Fragment() {
@@ -26,7 +25,7 @@ class MyOrdersFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MyOrdersViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MyOrdersViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
